6f00b83260b9d15b60d3b161975bd92cf65508a7
zayedhamadi-dev <130105189+zayedhamadi@users.noreply.github.com>
2023-08-04 16:01:10 +0100

    Add files via upload

---
+++ crud/src/main/java/com/example/crud/entity/MyBookList.java
@@ -0,0 +1,71 @@
+package com.example.crud.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+
+@Data
+
+@Table(name = "MyBooks")
+public class MyBookList  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "price")
+    private String price;
+
+
+    public MyBookList(int id, String name, String author, String price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public MyBookList() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
+
---
+++ crud/src/main/java/com/example/crud/service/BookService.java
@@ -0,0 +1,31 @@
+package com.example.crud.service;
+
+import com.example.crud.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.crud.repository.BookRepository;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+
+        @Autowired
+        private BookRepository bRepo;
+
+        public void save(Book b) {
+            bRepo.save(b);
+        }
+
+        public List<Book> getAllBook(){
+            return bRepo.findAll();
+        }
+
+        public Book getBookById(int id) {
+            return bRepo.findById(id).get();
+        }
+        public void deleteById(int id) {
+            bRepo.deleteById(id);
+        }
+    }
